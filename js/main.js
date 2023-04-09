5e461db8eff557e2da8cd3e27c1beec3aa87be15
Koshkaflo <glebovanna94@gmail.com>
2023-04-09 14:36:58 +0300

    add dark mode to css

--- js/main.js
+++ js/main.js
@@ -2,4 +2,5 @@ const btnDarkMode = document.querySelector('.dark-mode-btn');
 
 btnDarkMode.onclick = function () {
     btnDarkMode.classList.toggle('dark-mode-btn__active');
+    document.body.classList.toggle('dark');
 };
